1a521d1577a0ca5dd3c9ebb2a9bd4f0543074057
mgdesaix <mgdesaix@gmail.com>
2024-02-06 09:51:06 -0700

    updated blast summary format

--- scripts/blast_summary.R
+++ scripts/blast_summary.R
@@ -8,11 +8,14 @@ blast <- read_table("./03_results/04_summary/blast_summary.txt",
 
 
 blast_sp <- blast %>%
-  group_by(Sample, Species) %>%
-  summarize(Depth = sum(Depth),
+  group_by(Sample, Kingdom, Phylum, Class, Order,
+           Family, Genus, Species) %>%
+  summarize(Depth_sum = sum(Depth),
+            # N_OTUs = n(),
             .groups = "drop") %>%
-  filter(Depth > 5) %>%
-  arrange(Sample, desc(Depth))
+  # filter(Depth > 5) %>%
+  arrange(Sample, desc(Depth_sum)) %>%
+  pivot_wider(names_from = Sample, values_from = Depth_sum)
 write_csv(x = blast_sp,
           file = "./03_results/04_summary/blast_species_summary.txt")
 
